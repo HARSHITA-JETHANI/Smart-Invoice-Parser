3cccabb539f6feca186fc58cb8b1241aadba150b
HARSHITA-JETHANI <harshitajethani29@gmail.com>
2026-06-20 21:59:51 +0530

    file validation and error handling

--- middleware/upload.middleware.js
+++ middleware/upload.middleware.js
@@ -10,8 +10,24 @@ const storage = multer.diskStorage({
     }
 });
 
+const allowedTypes = ["application/pdf","image/png","image/jpeg"];
+
+const fileFilter = (req, file, cb) => {
+
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only PDF, PNG, JPG and JPEG files are allowed"));
+    }
+
+};
+
 const upload = multer({
-    storage
+    storage,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    },
+    fileFilter
 });
 
 module.exports = upload;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const invoiceRoutes = require("./routes/invoice.routes");
 const logger = require("./middleware/logger.middleware");
 const uploadRoutes = require("./routes/upload.routes");
+const errorHandler = require("./middleware/error.middleware");
 
 const app = express();
 
@@ -13,6 +14,12 @@ app.use(logger);
 app.use("/api/invoices", invoiceRoutes);
 
 app.use("/api/upload", uploadRoutes);
+
+app.use(errorHandler);
+
+
+
+
 
 app.get("/", (req, res) => {
     res.send("Smart Invoice Parser Backend Running");
