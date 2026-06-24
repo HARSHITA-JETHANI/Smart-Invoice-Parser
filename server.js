19f90910e248640cca8ba30410c056297401f490
HARSHITA-JETHANI <harshitajethani29@gmail.com>
2026-06-24 21:06:23 +0530

    .

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const errorHandler = require("./middleware/error.middleware");
 const app = express();
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
